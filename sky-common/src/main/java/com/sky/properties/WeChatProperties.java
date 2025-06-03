e0a151263aa60c24004d2198dc9740ef3a9b5f46
jun <fitjun@foxmail.com>
2025-06-03 20:22:54 +0800

    用户微信登陆业务代码

--- sky-common/src/main/java/com/sky/properties/WeChatProperties.java
+++ sky-common/src/main/java/com/sky/properties/WeChatProperties.java
@@ -3,13 +3,13 @@ package com.sky.properties;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "sky.wechat")
 @Data
 public class WeChatProperties {
+
     private String appid; //小程序的appid
     private String secret; //小程序的秘钥
     private String mchid; //商户号
